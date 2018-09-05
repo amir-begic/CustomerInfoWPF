ccea06fd8fc359b8434763111520ecfa25423588
Amir Begic <amir.begic@namics.com>
2018-09-05 21:44:15 +0200

    Completed task 4: Included Datasource.dll and CustomersOrders.xml in Solution and display the first customer OnLoad.

--- CustomerWPF/MainWindow.xaml.cs
+++ CustomerWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Datasource;
 
 namespace CustomerWPF
 {
@@ -20,15 +21,35 @@ namespace CustomerWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Data data;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            this.data = Data.Load();
+            DisplayCustomer(0);
+
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
             Application.Current.Shutdown();
+        }
+
+        private void DisplayCustomer(int index)
+        {
+            this.CustomerIdTextBox.Text = data.Customers.CustomerList[index].CustomerID;
+            this.CompanyNameTextBox.Text = data.Customers.CustomerList[index].CompanyName;
+            this.ContactNameTextBox.Text = data.Customers.CustomerList[index].ContactName;
+            this.ContactTitleTextBox.Text = data.Customers.CustomerList[index].ContactTitle;
+            this.PhoneTextBox.Text = data.Customers.CustomerList[index].Phone;
+            this.FaxTextBox.Text = data.Customers.CustomerList[index].Fax;
+            this.BirthdayDatePicker.SelectedDate = data.Customers.CustomerList[index].Birthday;
         }
     }
 }
